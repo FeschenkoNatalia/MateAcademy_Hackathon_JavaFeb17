272585f71926a9d70b12210312fae00e1b3ac738
mypro11 <iryna1anyri@gmail.com>
2017-05-20 14:42:54 +0300

    Add home page

--- src/main/java/com/mate_academy/social_network/controller/HomeController.java
+++ src/main/java/com/mate_academy/social_network/controller/HomeController.java
@@ -1,4 +1,22 @@
 package com.mate_academy.social_network.controller;
 
+import com.mate_academy.social_network.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HomeController {
+
+    private UserService userService;
+
+
+    @RequestMapping("/user_page")
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "logout", required = false) String logout,
+                        Model model){
+
+        return "homePage";
+    }
 }
