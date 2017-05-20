ff262b7bad37d1ab6564ef8404a19056a75cecbd
Melnichenkodo <Melnichenkodo@gmail.com>
2017-05-20 18:53:35 +0300

    -bug fixing (save user to db social.users)

--- src/main/java/com/mate_academy/social_network/model/User.java
+++ src/main/java/com/mate_academy/social_network/model/User.java
@@ -1,10 +1,11 @@
 package com.mate_academy.social_network.model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-@Table(name = "users")
-public class User {
+@Table(name = "social.users")
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
