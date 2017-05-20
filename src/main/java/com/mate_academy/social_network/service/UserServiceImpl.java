9a0a11d757671ac2e5e55c9294f42bdf07ae61c0
mypro11 <iryna1anyri@gmail.com>
2017-05-20 17:07:16 +0300

    User Service

--- src/main/java/com/mate_academy/social_network/service/UserServiceImpl.java
+++ src/main/java/com/mate_academy/social_network/service/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.mate_academy.social_network.service;
 import com.mate_academy.social_network.dao.UserDao;
 import com.mate_academy.social_network.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
