65aff65e68e30368203670fac81f9b3e23514b5c
Melnichenkodo <Melnichenkodo@gmail.com>
2017-05-21 10:38:57 +0300

    -edit table friends

--- src/main/java/com/mate_academy/social_network/service/UserServiceImpl.java
+++ src/main/java/com/mate_academy/social_network/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.mate_academy.social_network.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class UserServiceImpl implements UserService {
